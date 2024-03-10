a0683c36458d35f5c716c698d4df84809c32688f
Berke <berketunckal@gmail.com>
2024-03-10 19:46:13 +0300

    PID Controller.py Update

--- PyGamePID.py
+++ PyGamePID.py
@@ -29,7 +29,6 @@ class PIDController:
         self.ki = ki
         self.kd = kd
         self.setpoint = setpoint
-        
         self.prev_error = 0
         self.integral = 0
     
@@ -37,9 +36,7 @@ class PIDController:
         error = self.setpoint - current_value
         self.integral += error
         derivative = error - self.prev_error
-        
         output = self.kp * error + self.ki * self.integral + self.kd * derivative
-        
         self.prev_error = error
         return output
 
